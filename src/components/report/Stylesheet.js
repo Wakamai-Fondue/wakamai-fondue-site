81ab59efe51b9938df5d1696e26697d8f9580fad
Roel Nieskens <roel@pixelambacht.nl>
2021-01-28 21:17:46 +0100

    Add download button for the CSS.
    (Needs the engine to return the new "slug")

--- src/components/report/Stylesheet.js
+++ src/components/report/Stylesheet.js
@@ -7,7 +7,28 @@ export default {
 	},
 	data() {
 		return {
-			css: this.font.cssString
+			css: this.font.cssString,
+			fontname: this.font.summary["Font name"]
 		};
+	},
+	methods: {
+		getFontCSSLink: function() {
+			const filename = `${this.font.slug}-wakamai-fondue.css`;
+			let temp = document.createElement("a");
+			temp.setAttribute(
+				"href",
+				"data:text/plain;charset=utf-8," +
+					encodeURIComponent(this.font.cssString)
+			);
+			temp.setAttribute("download", filename);
+
+			if (document.createEvent) {
+				let event = document.createEvent("MouseEvents");
+				event.initEvent("click", true, true);
+				temp.dispatchEvent(event);
+			} else {
+				temp.click();
+			}
+		}
 	}
 };
