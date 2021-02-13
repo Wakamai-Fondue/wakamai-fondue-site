d4d4b887649fe4d203c581f1dcad425806488a75
Roel Nieskens <roel@pixelambacht.nl>
2021-02-13 13:04:29 +0100

    Add sticky button to variable tester as well
    Adding this to the current PR as this has the new styles for
    the sticky buttons

--- src/components/report/Variable.vue
+++ src/components/report/Variable.vue
@@ -1,14 +1,26 @@
 <template>
 	<section class="variable" id="variable">
 		<h2 class="section-title">Variable</h2>
-		<div class="content">
-			<div
-				contenteditable
-				class="variable-tester"
-				:style="variableStyles | inlinestyle"
-			>
-				The melting cheese & bread explode in a quick wave of joy: “1,
-				2, 3… zen!”
+		<div class="content variable-tester-container">
+			<div class="variable-tester" :class="{ sticky }">
+				<p
+					spellcheck="false"
+					contenteditable
+					:style="variableStyles | inlinestyle"
+				>
+					The melting cheese & bread explode in a quick wave of joy:
+					“1, 2, 3… zen!”
+				</p>
+				<button
+					contenteditable="false"
+					type="button"
+					alt="Pin text"
+					class="sticky-button"
+					:class="{ sticky }"
+					@click="sticky = !sticky"
+				>
+					📌
+				</button>
 			</div>
 			<VariableControls
 				:font="font"
