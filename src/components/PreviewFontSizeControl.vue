da433f8300936c8e0dd391f2e185f2f7a687fe15
Roel Nieskens <roel@pixelambacht.nl>
2025-10-09 22:32:29 +0200

    Add a little bit of paint to the slider

--- src/components/PreviewFontSizeControl.vue
+++ src/components/PreviewFontSizeControl.vue
@@ -40,9 +40,16 @@ export default {
 <style scoped>
 .preview-font-size-control {
 	position: fixed;
+	right: 0;
 	z-index: 1;
 	display: flex;
 	align-items: center;
 	gap: var(--small-margin);
+	background: var(--yellow);
+	padding: 1em;
+}
+
+.preview-font-size-control input {
+	width: min(15vw, 200px);
 }
 </style>
