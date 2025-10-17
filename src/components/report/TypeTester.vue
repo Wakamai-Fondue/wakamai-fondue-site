9099b391e75e1737b1ee13c3f8ce55d60b4fd3c9
Roel Nieskens <roel@pixelambacht.nl>
2025-10-17 16:54:46 +0200

    Use pangram for generic tester

--- src/components/report/TypeTester.vue
+++ src/components/report/TypeTester.vue
@@ -34,25 +34,15 @@
 				>
 					{{ customText }}
 				</p>
-				<p spellcheck="false" dir="auto" contenteditable v-else>
-					Brillat Savarin’s Fondue (an excellent recipe)
-					<br />
-					<br />
-					Ingredients: eggs, cheese, butter, pepper and salt. Take the
-					same number of eggs as there are guests; weigh the eggs in
-					the shell, allow a third of their weight in Gruyère cheese,
-					and a piece of butter one-sixth of the weight of the cheese.
-					Break the eggs into a basin, beat them well; add the cheese,
-					which should be grated, and the butter, which should be
-					broken in small pieces. Stir these ingredients together with
-					a wooden spoon; put the mixture into a lined saucepan, place
-					it over the fire, and stir until the substance is thick and
-					soft. Put in a little salt, according to the age of the
-					cheese, and a good sprinkling of pepper, and serve the
-					fondue on a very hot silver or metal plate. Do not allow the
-					fondue to remain on the fire after the mixture is set, as,
-					if it boils, it will be entirely spoilt. 0123456789
-					!@#$%^&*(),.?
+				<p
+					dir="auto"
+					contenteditable="plaintext-only"
+					spellcheck="false"
+					autocorrect="off"
+					v-else
+				>
+					The melting cheese & bread explode in a quick wave of joy:
+					"1, 2, 3… zen!"
 				</p>
 			</div>
 
