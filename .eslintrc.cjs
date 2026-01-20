26783ea48cb16b29703245db2fee18264896a143
Roel Nieskens <roel@pixelambacht.nl>
2026-01-20 22:57:18 +0100

    Fix linter
    After our recent changes in the previous PR

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
 	extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
 	parserOptions: {
 		parser: "@babel/eslint-parser",
+		requireConfigFile: false,
 	},
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
