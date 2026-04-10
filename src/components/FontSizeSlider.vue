2131da124af7af48f4957831e59c247a539b2bc1
Roel Nieskens <roel@pixelambacht.nl>
2026-04-10 12:08:30 +0200

    Update font size slider styling
    To put extra emphasis on it, we hide it when no fontsizechangeable text
    is in view

--- src/components/FontSizeSlider.vue
+++ src/components/FontSizeSlider.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="font-size-slider">
+	<div class="font-size-slider" :class="{ visible }">
 		<label for="font-size-slider">Font size</label>
 		<input
 			id="font-size-slider"
@@ -18,6 +18,12 @@
 import { usePreferences } from "@/composables/usePreferences.js";
 
 export default {
+	props: {
+		visible: {
+			type: Boolean,
+			default: true,
+		},
+	},
 	setup() {
 		const { fontSize, setFontSize } = usePreferences();
 		return {
@@ -42,9 +48,15 @@ export default {
 	align-items: center;
 	gap: var(--small-margin);
 	font-size: 1rem;
-	background: var(--offwhite);
-	padding: 0.75em 1em;
+	padding: 0.75em 0 0.75em 1em;
 	height: 100%;
+	accent-color: var(--green);
+	opacity: 0;
+	transition: opacity 0.2s ease;
+}
+
+.font-size-slider.visible {
+	opacity: 1;
 }
 
 .font-size-slider input {
